2281c8f0b7b4f9e21285ac03cd93cf5b001a8daf
Andrew <andrey777-novikov777@yandex.ru>
2023-05-25 21:38:57 +0300

    +Логгер, +Подключение к бд без ошибок

---
+++ newScripts/logger.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+from colorama import init, Fore, Style
+from enum import Enum
+init(autoreset=True)
+
+
+class Status(Enum):
+    WARNING = f"{Style.BRIGHT}{Fore.YELLOW}WARNING{Style.RESET_ALL}"
+    INFO = f"{Style.BRIGHT}{Fore.CYAN}INFO   {Style.RESET_ALL}"
+    SUCCESS = f"{Style.BRIGHT}{Fore.GREEN}SUCCESS{Style.RESET_ALL}"
+    FAILURE = f"{Style.BRIGHT}{Fore.RED}FAILURE{Style.RESET_ALL}"
+
+
+
+class Log:
+
+    def __init__(self) -> None:
+        raise NotImplementedError(f"Cannot create instance of {type(self)} class")
+
+    # "Движок" логгера
+    @staticmethod
+    def display(status: Status):
+        """
+            Декоратор, ставящий перед возвращаемым функции текстом
+            временную отметку и статус "`status`", после выводя в `stdout`
+        """
+        def display(function):
+            def wrapper(text: str) -> str:
+                timestamp = datetime.now().strftime("%Y-%m-%d %X")
+                timestamp_string = f"{Style.BRIGHT}{Fore.BLACK}{timestamp}"
+                status_string = f"{timestamp_string} {status.value}  "
+                print(status_string + function(text))
+            return wrapper
+        return display
+
+    # Методы для логгера
+    @display(Status.WARNING)
+    def warning(text: str) -> str:
+        return text
+
+    @display(Status.INFO)
+    def info(text: str) -> str:
+        return text
+
+    @display(Status.SUCCESS)
+    def success(text: str) -> str:
+        return text
+
+    @display(Status.FAILURE)
+    def failure(text: str) -> str:
+        return text
+
+
+if __name__ == "__main__":
+    Log.success("Hi!")
--- newScripts/main.py
+++ newScripts/main.py
@@ -1,36 +1,62 @@
 import os
 import discord
 import environ_loader
-from database import Database
+import asyncio
+import sys
+from logger import Log
+import asyncpg
 from discord.ext import commands
 
 permissions = discord.Intents()
 permissions.message_content = True
 permissions.messages = True
 permissions.reactions = True
-
-db = Database()
 bot = commands.Bot(command_prefix=os.getenv("BOT_PREFIX"), intents=permissions)
 
 
 @bot.event
-async def on_ready():
-    print("Bot is ready!")
+async def on_connect():
+    Log.success("Connected to the discord bot!")
+    Log.info("Connecting to the PostgreSQL database...")
+    bot.pool = await asyncpg.create_pool()
+    Log.success("Successfully connected to the db!")
 
 
 @bot.event
-async def on_disconnect():
-    await db.close()
+async def on_ready():
+    Log.success("Bot started!")
 
 
 @bot.command()
 async def test(ctx: commands.Context):
-    async with db.pool.acquire() as conn:
-        async with conn.transaction():
-            data = await conn.fetch("SELECT * FROM person LIMIT 5")
-
+    async with bot.pool.acquire() as conn:
+        data = await conn.fetch("SELECT * FROM person LIMIT 4")
+        print(data)
     await ctx.channel.send("Registered!")
 
 
+@bot.command()
+@commands.is_owner()
+async def shutdown(ctx: commands.Context):
+    try:
+        sys.exit(0)
+    except Exception:  # До сих пор не блочит все asyncio исключения
+        pass
+    finally:
+        Log.info("Bot offline")
+
+
 if __name__ == "__main__":
-    bot.run(os.getenv("BOT_TOKEN"))
+    # if os.name == "nt":
+    #     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+    try:
+        Log.info("Launching discord bot...")
+        bot.run(os.getenv("BOT_TOKEN"))
+    except discord.errors.LoginFailure as e:
+        Log.failure(str(e)), sys.exit(1)
+    finally:
+        if getattr(bot, "pool", None):
+            Log.info("Closing the database connection pool")
+            asyncio.new_event_loop().run_until_complete(bot.pool.close())
+            Log.success("Database connection pool closed!")
